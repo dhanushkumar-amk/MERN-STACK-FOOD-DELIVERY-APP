f2fc7e382bd1d950a4f63537a0a7b5a4ff0bacfb
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-01 09:28:15 +0530

    modifed

--- Backend/controller/OrderController.js
+++ Backend/controller/OrderController.js
@@ -1,21 +1,54 @@
-import orderModel from '../models/OrderModel.js';
-import userModel from '../models/UserModel.js';
+import orderModel from '../models/OrderModel.js';import userModel from '../models/UserModel.js';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRETKEY);
 /** placing user order from frontend */
 const placeOrder = async (req, res) => {
+  const frontend_url = 'http://localhost:5137';
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
       item: req.body.items,
       amount: req.body.amount,
-      address: req.boy.address,
+      address: req.body.address,
     });
 
     await newOrder.save();
     await userModel.findByIdAndUpdate(req.body.userId, {cartData: {}});
-  } catch (error) {}
+
+    const line_items = req.body.items.map((item) => ({
+      price_data: {
+        currency: 'inr',
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price * 100 * 80,
+      },
+      quantity: item.quantity,
+    }));
+
+    line_items.push({
+      price_data: {
+        currency: 'inr',
+        product_data: {
+          name: 'Delivery Charges',
+        },
+        unit_amount: 3 * 100 * 80,
+      },
+      quantity: 1,
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: line_items,
+      mode: 'payment',
+      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+    });
+    res.json({success: true, session_url: session.url});
+  } catch (error) {
+    console.log(error);
+    res.json({success: false, message: 'Error'});
+  }
 };
 
 export {placeOrder};
--- Frontend/src/Pages/Place-Order/PlaceOrder.jsx
+++ Frontend/src/Pages/Place-Order/PlaceOrder.jsx
@@ -1,41 +1,107 @@
-import {useContext} from 'react';
-import './PlaceOrder.css';
+import {useContext, useState} from 'react';import './PlaceOrder.css';
 import {StoreContext} from '../../Context/StoreContext';
+import axios from 'axios';
 const PlaceOrder = () => {
-  const {getTotalCartAmount} = useContext(StoreContext);
+  const {getTotalCartAmount, token, food_list, cartItems, url} =
+    useContext(StoreContext);
+
+  const [data, setData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    street: '',
+    city: '',
+    state: '',
+    pincode: '',
+    country: '',
+    phone: '',
+  });
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({...data, [name]: value}));
+  };
+
+  const placeOrder = async (event) => {
+    event.preventDefault();
+    let orderItems = [];
+    food_list.map((item) => {
+      if (cartItems[item._id] > 0) {
+        let itemInfo = item;
+        itemInfo['quantity'] = cartItems[item._id];
+        orderItems.push(itemInfo);
+      }
+    });
+    let orderData = {
+      address: data,
+      items: orderItems,
+      amount: getTotalCartAmount() + 3,
+    };
+    let response = await axios.post(url + '/api/order/place', orderData, {
+      headers: {token},
+    });
+    if (response.data.success) {
+      const {session_url} = response.data;
+      window.location.replace(session_url);
+    } else {
+      alert('Error');
+    }
+  };
+
   return (
-    <form className='place-order'>
+    <form
+      onSubmit={placeOrder}
+      className='place-order'>
       <div className='place-order-left'>
         <p className='title'>Delivery Information</p>
         <div className='multi-fields'>
           <input
+            name='firstName'
+            onChange={onChangeHandler}
+            value={data.firstName}
             type='text'
             placeholder='First Name'
             required
           />
           <input
+            name='lastName'
+            onChange={onChangeHandler}
+            value={data.lastName}
             type='text'
             placeholder='Last Name'
             required
           />
         </div>
         <input
+          name='email'
+          onChange={onChangeHandler}
+          value={data.email}
           type='email'
           placeholder='Email address'
           required
         />
         <input
+          name='street'
+          onChange={onChangeHandler}
+          value={data.street}
           type='text'
           placeholder='street'
           required
         />
         <div className='multi-fields'>
           <input
+            name='city'
+            onChange={onChangeHandler}
+            value={data.city}
             type='text'
             placeholder='City'
             required
           />
           <input
+            name='state'
+            onChange={onChangeHandler}
+            value={data.state}
             type='text'
             placeholder='State'
             required
@@ -43,17 +109,26 @@ const PlaceOrder = () => {
         </div>
         <div className='multi-fields'>
           <input
+            name='pincode'
+            onChange={onChangeHandler}
+            value={data.pincode}
             type='text'
-            placeholder='Pin Code'
+            placeholder='Pincode / zipcode'
             required
           />
           <input
             type='text'
+            name='country'
+            onChange={onChangeHandler}
+            value={data.country}
             placeholder='country'
             required
           />
         </div>
         <input
+          name='phone'
+          onChange={onChangeHandler}
+          value={data.phone}
           type='text '
           placeholder='Phone'
         />
@@ -79,7 +154,7 @@ const PlaceOrder = () => {
               </p>
             </div>
           </div>
-          <button>Proceed To Payment </button>
+          <button type='submit'>Proceed To Payment </button>
         </div>
       </div>
     </form>
