be6fedfa3e38fa344f6c254947d29cce9c48600d
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-18 12:32:29 +0530

    modified

--- Backend/server.js
+++ Backend/server.js
@@ -8,15 +8,16 @@ import orderRouter from './Routes/OrderRoute.js';
 // App config
 
 const app = express();
-const port = 4000;
+const port = process.env.port || 4000;
 
 // middleware
-
 app.use(express.json());
 app.use(cors());
 
+
 // db connection
 connectDB();
+
 
 // Api End point
 app.use('/api/food', foodRouter);
@@ -25,9 +26,11 @@ app.use('/api/user', userRouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/order', orderRouter);
 
+
 app.get('/', (req, res) => {
   res.send('API working');
 });
+
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
