4fd32390303556755afd28aad7ab0491d6e8e6d5
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-06-18 17:32:13 +0530

    modified

--- Backend/server.js
+++ Backend/server.js
@@ -5,32 +5,28 @@ import userRouter from './Routes/UserRoute.js';
 import 'dotenv/config.js';
 import cartRouter from './Routes/CartRoute.js';
 import orderRouter from './Routes/OrderRoute.js';
+
 // App config
-
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000; // use logical OR (||) instead of bitwise OR (|)
 
-// middleware
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-
-// db connection
+// DB connection
 connectDB();
 
-
-// Api End point
+// API Endpoints
 app.use('/api/food', foodRouter);
 app.use('/images', express.static('Uploads'));
 app.use('/api/user', userRouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/order', orderRouter);
 
-
 app.get('/', (req, res) => {
   res.send('API working');
 });
-
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
